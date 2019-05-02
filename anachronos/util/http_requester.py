4160e723f37ce928e57a97a00ec0082f88e7adc1
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-05-02 10:04:05 -0400

    headers in HttpRequester

--- anachronos/util/http_requester.py
+++ anachronos/util/http_requester.py
@@ -1,33 +1,52 @@
 import requests
 
+from anachronos.compat.jivago_streams import Stream
+
 
 class HttpRequester(object):
 
-    def __init__(self, host='localhost', port=4000, path=''):
+    def __init__(self, host='localhost', port=4000, path='', default_arguments: dict = None):
         self.path = path
         self.host = host
         self.port = port
 
+        if default_arguments is None:
+            default_arguments = {}
+
+        self.default_arguments = default_arguments
+
     def with_path(self, sub_path: str) -> "HttpRequester":
-        return HttpRequester(self.host, self.port, sub_path)
+        return HttpRequester(self.host, self.port, sub_path, default_arguments=self.default_arguments)
+
+    def append_path(self, sub_path: str) -> "HttpRequester":
+        return HttpRequester(self.host, self.port, self.path + sub_path, default_arguments=self.default_arguments)
 
     def get(self, path: str, *args, **kwargs):
-        return requests.get(self.get_full_url(path), *args, **kwargs)
+        return requests.get(self.get_full_url(path), *args, **self._join_arg_dict(self.default_arguments, kwargs))
 
     def post(self, path: str, *args, **kwargs):
-        return requests.post(self.get_full_url(path), *args, **kwargs)
+        return requests.post(self.get_full_url(path), *args, **self._join_arg_dict(self.default_arguments, kwargs))
 
     def put(self, path: str, *args, **kwargs):
-        return requests.put(self.get_full_url(path), *args, **kwargs)
+        return requests.put(self.get_full_url(path), *args, **self._join_arg_dict(self.default_arguments, kwargs))
 
     def patch(self, path, *args, **kwargs):
-        return requests.patch(self.get_full_url(path), *args, **kwargs)
+        return requests.patch(self.get_full_url(path), *args, **self._join_arg_dict(self.default_arguments, kwargs))
 
     def head(self, path, **kwargs):
-        return requests.head(self.get_full_url(path), **kwargs)
+        return requests.head(self.get_full_url(path), **self._join_arg_dict(self.default_arguments, kwargs))
 
     def options(self, path, **kwargs):
-        return requests.options(self.get_full_url(path), **kwargs)
+        return requests.options(self.get_full_url(path), **self._join_arg_dict(self.default_arguments, kwargs))
 
     def get_full_url(self, path):
         return f"{self.host}:{self.port}{self.path}{path}"
+
+    def _join_arg_dict(self, dict1, dict2) -> dict:
+        keys = list(Stream.of(dict1, dict2).map(dict.keys).toSet())
+        values = Stream(keys) \
+            .map(lambda key: (dict1.get(key), dict2.get(key))) \
+            .map(lambda x: Stream(x).filter(lambda y: y is not None).sum()) \
+            .toList()
+
+        return Stream.zip(keys, values).toDict()
