d283fa50953761fd010f613063dd3e97f801baad
Kento Lauzon <kento.lauzon@ligature.ca>
2022-09-13 06:43:28 +0100

    remove dependency on setuptools-git-version

--- setup.py
+++ setup.py
@@ -8,19 +8,31 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+def read_version_from_git() -> str:
+    try:
+        version_string = check_output(git_version_command.split()).decode('utf-8').strip().split("-")
+        dirty = version_string[1] != "0"
+        if dirty:
+            return f"{version_string[0]}+{version_string[2]}"
+        return version_string[0]
+
+    except:
+        return "0.0.0"
+
 if path.exists(here + "/.git"):
     init_file = None
     with open(path.join(here, 'anachronos/__init__.py'), 'r', encoding='utf-8') as f:
         init_file = f.read()
     if "@@VERSION@@" in init_file:
         with open(path.join(here, 'anachronos/__init__.py'), 'w', encoding='utf-8') as f:
-            f.write(init_file.replace("@@VERSION@@",
-                                      check_output(git_version_command.split()).decode('utf-8').strip().split("-")[0]))
+            f.write(init_file.replace("@@VERSION@@", read_version_from_git()))
+
+import anachronos
 
 setup(
     name='anachronos',
-    version_format='{tag}',
-    setup_requires=['setuptools-git-version'],
+    version=anachronos.__version__,
+    setup_requires=[],
     description='A testing framework for testing frameworks.',
     long_description=long_description,
     long_description_content_type='text/markdown',
